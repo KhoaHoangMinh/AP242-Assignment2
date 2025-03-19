14a46c489ba9a3b031803b15353303a60c702acc
KhoaHoangMinh <hoangkhoa2015@gmail.com>
2025-03-19 10:34:43 +0700

    Add checkRegister()

--- src/main/java/com/myproject/StockFeeder.java
+++ src/main/java/com/myproject/StockFeeder.java
@@ -1,22 +1,43 @@
 package com.myproject;
 
+import jdk.jpackage.internal.Log;
+
 import java.util.*;
 
 public class StockFeeder {
     private List<Stock> stockList = new ArrayList<>();
     private Map<String, List<StockViewer>> viewers = new HashMap<>();
+    // Each stock has a list of viewers that want to receive updates
+    // When the price changes, notify(StockPrice stockPrice) will look up the stock code and notify all viewers
     private static StockFeeder instance = null;
+
+    public boolean checkRegister(String code, StockViewer stockViewer) {
+        boolean stockExists = false;
+        boolean viewerExists = false;
+
+        for(Stock stock : stockList) {
+            if(stock.getCode().equals(code)) {
+                stockExists = true;
+                break;
+            }
+        }
+        List<StockViewer> viewerList = viewers.get(code);
+        if(viewerList != null) {viewerExists = viewerList.contains(stockViewer);}
+        return stockExists && viewerExists;
+    }
 
     // TODO: Implement Singleton pattern
     private StockFeeder() {}
 
     public static StockFeeder getInstance() {
         // TODO: Implement Singleton logic
-        return null;
+        if(instance == null) {instance = new StockFeeder();}
+        return instance;
     }
 
     public void addStock(Stock stock) {
         // TODO: Implement adding a stock to stockList
+        stockList.add(stock);
     }
 
     public void registerViewer(String code, StockViewer stockViewer) {
