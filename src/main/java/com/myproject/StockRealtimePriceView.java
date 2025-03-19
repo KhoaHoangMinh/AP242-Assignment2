a26feb021b6105a23c0c84c5bc84d69a2211b875
KhoaHoangMinh <hoangkhoa2015@gmail.com>
2025-03-19 20:28:32 +0700

    Add getters for hashMap

--- src/main/java/com/myproject/StockRealtimePriceView.java
+++ src/main/java/com/myproject/StockRealtimePriceView.java
@@ -9,5 +9,16 @@ public class StockRealtimePriceView implements StockViewer {
     @Override
     public void onUpdate(StockPrice stockPrice) {
         // TODO: Implement logic to check if price has changed and log it
+        String stockCode = stockPrice.getCode();
+        double newPrice = stockPrice.getAvgPrice();
+        Double lastPrice = lastPrices.get(stockCode);
+
+        if(lastPrice == null || newPrice != lastPrice) {
+            Logger.logRealtime(stockCode, newPrice);
+            lastPrices.put(stockCode, newPrice);
+        }
+    }
+    public Map<String, Double> getLastPrices() {
+        return lastPrices;
     }
 }
