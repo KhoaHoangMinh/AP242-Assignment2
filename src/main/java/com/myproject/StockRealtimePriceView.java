d09f6df9a031643429e94573e5f0a894b2a15558
KhoaHoangMinh <hoangkhoa2015@gmail.com>
2025-03-23 15:29:04 +0700

    Fix lastPrice

--- src/main/java/com/myproject/StockRealtimePriceView.java
+++ src/main/java/com/myproject/StockRealtimePriceView.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public class StockRealtimePriceView implements StockViewer {
     private final Map<String, Double> lastPrices = new HashMap<>();
+    private Double backupPrice;
 
     @Override
     public void onUpdate(StockPrice stockPrice) {
@@ -13,9 +14,14 @@ public class StockRealtimePriceView implements StockViewer {
         double newPrice = stockPrice.getAvgPrice();
         Double lastPrice = lastPrices.get(stockCode);
 
-        if(lastPrice == null || newPrice != lastPrice) {
+        if(lastPrice == null) {
             Logger.logRealtime(stockCode, newPrice);
             lastPrices.put(stockCode, newPrice);
+            backupPrice = newPrice;
+        } else if(newPrice != backupPrice) {
+            Logger.logRealtime(stockCode, newPrice);
+            lastPrices.put(stockCode, backupPrice);
+            backupPrice = newPrice;
         }
     }
     public Map<String, Double> getLastPrices() {
